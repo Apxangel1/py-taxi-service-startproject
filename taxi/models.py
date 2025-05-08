93da62b4d429ce008258e6f3eab8c54d574630b5
Volodymyr Vitsenko <your@mail.com>
2025-05-08 13:52:51 +0200

    Taxi service app

--- taxi/models.py
+++ taxi/models.py
@@ -6,11 +6,17 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=100, unique=True)
     street = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Car(models.Model):
     model = models.CharField(max_length=100, blank=True, null=True)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='cars')
+
+    def __str__(self):
+        return f"{self.manufacturer.name} {self.model}"
 
 
 class Driver(AbstractUser):
